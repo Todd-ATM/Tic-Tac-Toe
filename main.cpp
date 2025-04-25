2c6e29f32ccdfae07b2059bfc0a9ec613953ab6e
Todd-ATM <ToddJr.Acrossthemountain@gmail.com>
2025-04-25 13:23:34 -0700

    added board class, with constructor initialization and draw function

--- main.cpp
+++ main.cpp
@@ -1,6 +1,39 @@
 #include <iostream>
 
+class Board {
+  public:
+    //constructor
+    Board() {
+        char value = '0';
+        //nested for loops to iterate, and assign value
+        //to each element in our 2d array
+        for (int i = 0; i < Row; i++) {
+            for (int j = 0; j < Col; j++) {
+                MainBoard[i][j] = value;
+            }
+        }
+    }
+
+   void DrawBoard() {
+    //nested for loop
+    for (int i = 0; i < Row; i++) {
+        for (int j = 0; j < Col; j++) {
+            std::cout << MainBoard[i][j] << " ";
+        }
+    std::cout << std::endl;
+    }
+    }
+
+  private:
+    int Row = 3;
+    int Col = 3;
+    char MainBoard[3][3];
+};
+
 int main() {
+    Board main;
+    main.DrawBoard();
     std::cout << "Hello World";
     return 0;
 }
+
